dc1795612a4e7e6bcb4f98d2e557f233ddeb4912
Jim Seconde <jim.seconde@googlemail.com>
2017-02-05 14:39:28 +0000

    My god, the to do list has actually made some progress. Started on add form

--- src/VisualiserBundle/Controller/ToDoController.php
+++ src/VisualiserBundle/Controller/ToDoController.php
@@ -4,6 +4,10 @@ namespace VisualiserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use VisualiserBundle\Model;
 
 class ToDoController extends Controller
