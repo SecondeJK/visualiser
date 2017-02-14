426c94467bbadb897066a2846bd2a3e7fccf2add
Jim Seconde <jim.seconde@googlemail.com>
2017-02-14 08:50:33 +0000

    Started to fixing this stupid menu

--- src/VisualiserBundle/Menu/Builder.php
+++ src/VisualiserBundle/Menu/Builder.php
@@ -12,17 +12,21 @@ class Builder implements ContainerAwareInterface
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
-
-        $menu->addChild('Home', array('route' => 'homepage'));
         
-        // create another menu item
+        $menu->addChild('Home', array('route' => 'homepage'));
         $menu->addChild('Visual Libraries', array('route' => 'homepage'));
 		$menu->addChild('Dashboard Examples', array('route' => 'visualiser_dashboard'));
+		
+		//$menu['Visual Libraries']->setParentAttributes(array('class' => 'dropdown-menu'));
 		$menu['Visual Libraries']->addChild('Google Charts', array('route' => 'visualiser_googlecharts'));
 		$menu['Visual Libraries']->addChild('d3js', array('route' => 'visualiser_d3js'));
 		$menu['Visual Libraries']->addChild('HighCharts', array('route' => 'visualiser_dashboard'));
-		//$menu['Visual Libraries']->setLinkAttribute('class', '');
 		$menu->addChild('App ToDo List', array('route' => 'visualiser_todo_index'));
+		
+		$menu->setChildrenAttributes(array('class' => 'nav navbar-nav'));
+
+		// some reverse engineering required
+		dump(get_class_methods($menu));
 		dump($menu);
         return $menu;	
     }
