a02abe8387e2cc37c624be57d8cfa5c91b92f1a8
Jim Seconde <jim.seconde@googlemail.com>
2017-09-22 17:19:34 +0100

    commit composer, added first test

--- tests/visualiserBundle/ToDoControllerTest.php
+++ tests/visualiserBundle/ToDoControllerTest.php
@@ -4,8 +4,16 @@ namespace VisualiserBundle\Tests\Controller;
 
 class ToDoControllerTest extends \PHPUnit\Framework\TestCase
 {
+	
+	public $subjectController;
+	
 	public function setUp()
 	{
-		
+		$this->subjectController = new \VisualiserBundle\Controller\ToDoController();
+	}
+	
+	public function testSetup()
+	{
+		$this->assertInstanceOf(\VisualiserBundle\Controller\ToDoController::class, $this->subjectController);
 	}
 }
